3b8ca390fa5dd673a39257c6e92ca308a41faed9
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-05-16 01:09:24 +0700

    Add New Model and Implement Checkpoint

--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -4,26 +4,46 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    internal enum driver
+    internal enum Driver
     {
         ai, player
     }
-    [SerializeField] driver mode;
+    [SerializeField] Driver mode = Driver.player;
     CarController car;
-    [SerializeField,Range(-1,1)]private float inputX, inputY;
+    [SerializeField, Range(-1, 1)] private float inputX, inputY;
+
+    //Time and Lap
+    public float BestLapTime { get; private set; } = Mathf.Infinity;
+    public float LastLapTime { get; private set; } = 0f;
+    public float CurrentLapTime { get; private set; } = 0f;
+    public int CurrentLap { get; private set; } = 0;
+
+    private float lapTimer;
+
+    //Cekpoint
+    private int lastCheckPassed = 0;
+    private Transform checkpointParent;
+    private int checkCount;
+    private int checkLayer;
+
+
     private void Awake()
     {
         car = GetComponent<CarController>();
+        FindCheckpoint();
     }
 
     private void Update()
     {
+        CurrentLapTime = lapTimer > 0 ? Time.time - lapTimer : 0;
+
+        //Change AI or Player Driving this vehicle
         switch (mode)
         {
-            case driver.ai:
-                ai();
+            case Driver.ai:
+                Ai();
                 break;
-            case driver.player:
+            case Driver.player:
                 player();
                 break;
         }
@@ -31,31 +51,27 @@ public class InputManager : MonoBehaviour
 
     void player()
     {
-        // if (Input.GetKey(KeyCode.W))
-        // {
-        //     car.CancelInvoke("DecelerateCar");
-        //     car.deceleratingCar = false;
-        //     car.GoForward();
-        // }
-        // if (Input.GetKey(KeyCode.S))
-        // {
-        //     car.CancelInvoke("DecelerateCar");
-        //     car.deceleratingCar = false;
-        //     car.GoReverse();
-        // }
+        if (Input.GetKey(KeyCode.W))
+        {
+            car.CancelInvoke("DecelerateCar");
+            car.deceleratingCar = false;
+            car.GoForward();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            car.CancelInvoke("DecelerateCar");
+            car.deceleratingCar = false;
+            car.GoReverse();
+        }
 
-        // if (Input.GetKey(KeyCode.A))
-        // {
-        //     car.TurnLeft();
-        // }
-        // if (Input.GetKey(KeyCode.D))
-        // {
-        //     car.TurnRight();
-        // }
-        inputX = Input.GetAxis("Horizontal");
-        inputY = Input.GetAxis("Vertical");
-        car.AiSteering(inputX);
-        car.AiSpeed(inputY);
+        if (Input.GetKey(KeyCode.A))
+        {
+            car.TurnLeft();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            car.TurnRight();
+        }
         if (Input.GetKey(KeyCode.Space))
         {
             car.CancelInvoke("DecelerateCar");
@@ -66,23 +82,69 @@ public class InputManager : MonoBehaviour
         {
             car.RecoverTraction();
         }
-        // if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
-        // {
-        //     car.ThrottleOff();
-        // }
-        // if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Space) && !car.deceleratingCar)
-        // {
-        //     car.InvokeRepeating("DecelerateCar", 0f, 0.1f);
-        //     car.deceleratingCar = true;
-        // }
-        // if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        // {
-        //     car.ResetSteeringAngle();
-        // }
+        if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+        {
+            car.ThrottleOff();
+        }
+        if (!Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Space) && !car.deceleratingCar)
+        {
+            car.InvokeRepeating("DecelerateCar", 0f, 0.1f);
+            car.deceleratingCar = true;
+        }
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            car.ResetSteeringAngle();
+        }
+        // inputX = Input.GetAxis("Horizontal");
+        // inputY = Input.GetAxis("Vertical");
+        // car.AiSteering(inputX);
+        // car.AiSpeed(inputY);
     }
-    void ai()
+    void Ai()
     {
         car.AiSteering(inputX);
         car.AiSpeed(inputY);
+    }
+
+    void FindCheckpoint()
+    {
+        checkpointParent = GameObject.Find("Checkpoint").transform;
+        checkCount = checkpointParent.childCount;
+        checkLayer = LayerMask.NameToLayer("Checkpoint");
+    }
+
+    void StartLap()
+    {
+        Debug.Log("Start New Lap");
+        CurrentLap++;
+        lastCheckPassed = 1;
+        lapTimer = Time.time;
+    }
+
+    void EndLap()
+    {
+        LastLapTime = Time.time - lapTimer;
+        BestLapTime = Mathf.Min(LastLapTime, BestLapTime);
+        Debug.Log("Lap " + CurrentLap + " Time: " + LastLapTime + " Best Time: " + BestLapTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer != checkLayer) return;
+
+        if (other.gameObject.name == "1")
+        {
+            if (lastCheckPassed == checkCount)
+            {
+                EndLap();
+            }
+            if (CurrentLap == 0 || lastCheckPassed == checkCount)
+            {
+                StartLap();
+            }
+            return;
+        }
+
+        if (other.gameObject.name == (lastCheckPassed + 1).ToString()) lastCheckPassed++;
     }
 }
