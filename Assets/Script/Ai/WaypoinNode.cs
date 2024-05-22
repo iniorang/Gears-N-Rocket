3ec2d03d21ca1a03746724cc7066fb41e0f94d29
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-05-22 17:57:50 +0700

    Adding AI

--- Assets/Script/Ai/WaypoinNode.cs
+++ Assets/Script/Ai/WaypoinNode.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class WaypoinNode : MonoBehaviour
 {
-    public WaypoinNode[] nextNode;
+    public List<Transform> node = new List<Transform>();
 
-    
-    public float minDistanceToWaypoint = 5f;
+    private void OnDrawGizmos()
+    {
+        Transform[] path = GetComponentsInChildren<Transform>();
+        node = new List<Transform>();
+        for (int i = 1; i < path.Length; i++)
+        {
+            node.Add(path[i]);
+        }
+        
+        for (int i = 0; i < node.Count; i++)
+        {
+            Vector3 currentNode = node[i].position;
+            Vector3 prevNode = Vector3.zero;
+
+            if (i != 0) prevNode = node[i - 1].position;
+            else if (i == 0) prevNode = node[node.Count - 1].position;
+
+            Gizmos.DrawLine(prevNode, currentNode);
+            Gizmos.DrawSphere(currentNode,1f);
+        }
+    }
 }
--- Assets/Script/InputManager.cs
+++ Assets/Script/InputManager.cs
@@ -3,22 +3,33 @@ using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 using System.Linq;
+using System.Collections.Generic;
 
 public class InputManager : MonoBehaviour
 {
     // Kontrol pemain atau pemain
     internal enum Driver
     {
-        ai, player
+        ai, Player
     }
-    [SerializeField] Driver mode = Driver.player;
+    [SerializeField] Driver mode = Driver.Player;
+    [SerializeField] Vector2 inputVector = Vector2.zero;
     CarController car;
-    [SerializeField]Vector2 inputVector = Vector2.zero;
+
+    [SerializeField] WaypoinNode waypoints;
+    [SerializeField] Transform currentNode;
+    [SerializeField] List<Transform> nodes = new List<Transform>();
+    [SerializeField][Range(0, 10)] int offset;
+    [SerializeField][Range(0, 10)] float steer;
 
     private void Awake()
     {
         car = GetComponent<CarController>();
-    
+    }
+
+    private void Start() {
+        waypoints = GameObject.FindGameObjectWithTag("Path").GetComponent<WaypoinNode>();
+        nodes = waypoints.node;
     }
 
     private void FixedUpdate()
@@ -29,13 +40,13 @@ public class InputManager : MonoBehaviour
             case Driver.ai:
                 Ai();
                 break;
-            case Driver.player:
-                player();
+            case Driver.Player:
+                Player();
                 break;
         }
     }
 
-    void player()
+    void Player()
     {
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
@@ -45,7 +56,41 @@ public class InputManager : MonoBehaviour
     }
     void Ai()
     {
+        calculateDistance();
+        inputVector.x = AiSteer();
+        inputVector.y = .5f;
+        car.MoveCar(inputVector.y);
         car.Steering(inputVector.x);
-        // car.AiSpeed(inputVector.y);
+    }
+
+    private float AiSteer()
+    {
+        Vector3 relative = transform.InverseTransformPoint(currentNode.transform.position);
+        relative /= relative.magnitude;
+        inputVector.x = relative.x / relative.magnitude * steer;
+        return inputVector.x;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        if (currentNode != null)
+            Gizmos.DrawSphere(currentNode.transform.position, 0.5f);
+    }
+
+    private void calculateDistance()
+    {
+        Vector3 position = gameObject.transform.position;
+        float distance = Mathf.Infinity;
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            Vector3 diff = nodes[i].position - position;
+            float currentDistance = diff.magnitude;
+            if (currentDistance < distance)
+            {
+                distance = currentDistance; // Update the minimum distance
+                currentNode = nodes[i];
+            }
+        }
     }
 }
