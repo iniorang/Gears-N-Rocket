36b91c3ef0e5d99b853e7bc1fab1f930ee3db65a
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-06-06 00:28:03 +0700

    Add UI, Track, and AI

---
+++ Assets/Script/UICOntroller.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject RacePanel;
+
+    public TextMeshProUGUI CurrentTimeUi;
+    public TextMeshProUGUI CurrentLapUi;
+    public TextMeshProUGUI MaxLapUi;
+    public TextMeshProUGUI CurrentPosUi;
+    public TextMeshProUGUI MaxOppentUi;
+
+    public RaceTime UpdateUI;
+    private float CurrentTime;
+    private int CurrentLap = -1;
+    private float MaxLap;
+    private float CurrentPos;
+    private float MaxOppent;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(UpdateUI == null)
+            return;
+
+        if(UpdateUI.CurrentLap != CurrentLap)
+        {
+            CurrentLap = UpdateUI.CurrentLap;
+            CurrentLapUi.text = $"{CurrentLap}";
+            // CurrentLapUi.text = "Test";
+        }
+
+        if(UpdateUI.CurrentLapTime != CurrentTime)
+        {
+            CurrentTime = UpdateUI.CurrentLapTime;
+            CurrentTimeUi.text = $"{(int)CurrentTime / 60} : {CurrentTime % 60:00.000}";
+        }
+    }
+}
